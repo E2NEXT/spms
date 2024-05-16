4925939d6a0aab069ba231a289adcfdf5b372f51
Abdullah-Salih <abdullahalsalih@gmail.com>
2024-05-16 15:19:56 +0300

    report v2

--- spms/sales_person_management_system/report/customer_payment_analysis_v2/customer_payment_analysis_v2.js
+++ spms/sales_person_management_system/report/customer_payment_analysis_v2/customer_payment_analysis_v2.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, aoai and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Customer Payment Analysis"] = {
+frappe.query_reports["Customer Payment Analysis V2"] = {
 	"filters": [
 		{
 			"fieldname": "company",
@@ -26,13 +26,26 @@ frappe.query_reports["Customer Payment Analysis"] = {
 			"options": "Customer Group",
 			"width": 100,
 		}, 
-
 		{
-			"fieldname": "show_zeros",
-			"label": __("Show Zeros"),
-			"fieldtype": "Check",
-			// "width": 100,
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": 100,
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -12),
+			"reqd": 1,
 		},
-		
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": 100,
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"on_change": function(query_report) {
+				if (!query_report.date_range_change) {
+					query_report.set_filter_value('date_range', '');
+				}
+			}
+		},
 	]
 };
